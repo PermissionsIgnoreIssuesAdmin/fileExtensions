a673f37184e093737d4a3c20c342e97d468ec7bb
devignoreissuesrepadmin <61156683+devignoreissuesrepadmin@users.noreply.github.com>
2023-10-26 15:13:17 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,3 +2,4 @@ val ok = 2;
 var test = "testing_";
 var regField = new RegExp('/\w+/' + test); // this stopped giving an issue after JSHint deprecation
 if (test == true) { alert("x"); }
+var api_token = "ebfb7ff0-aaaa-aaaa-aaaa-4fba17be410c";
