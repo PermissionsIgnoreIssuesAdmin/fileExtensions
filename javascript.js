9658cb7e2fe80ed5d58dc94d58203dc4f0f9ffbf
devignoreissuesrepadmin <61156683+devignoreissuesrepadmin@users.noreply.github.com>
2023-10-26 15:19:49 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,3 +3,5 @@ var test = "testing_";
 var regField = new RegExp('/\w+/' + test); // this stopped giving an issue after JSHint deprecation
 if (test == true) { alert("x"); }
 var secret = "ebfb7ff0-aaaa-aaaa-aaaa-4fba17be410c";
+'AWS_secret_KEY'="12ASD34qwe56CXZ78tyH10Tna543VBokN85RHCas"
+AWS_ACCESS_KEY_ID=AKIA0123456789ABCDEF
